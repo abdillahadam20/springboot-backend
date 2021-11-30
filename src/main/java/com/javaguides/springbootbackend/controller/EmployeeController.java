c0646dcf2ac8eea02684cb5ed3246d93d2e2e342
adamabdillah20 <adamabdillah20@gmail.com>
2021-11-30 10:41:22 +0700

    remove jdbc on application properties

--- src/main/java/com/javaguides/springbootbackend/controller/EmployeeController.java
+++ src/main/java/com/javaguides/springbootbackend/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import com.javaguides.springbootbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -22,5 +24,11 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    // create employee rest api
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 }
